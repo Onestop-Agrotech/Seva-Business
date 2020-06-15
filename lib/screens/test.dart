7ef81f82786868cebfc3e2876a5218bb9cdabc38
Vikas Prashar <vikasprashar99@gmail.com>
2020-06-15 17:34:15 +0530

    updated UI

--- lib/screens/test.dart
+++ lib/screens/test.dart
@@ -120,40 +120,91 @@ class _TestState extends State<Test> {
                     ],
                   );
                 } else {
-                  return Padding(
-                    padding: const EdgeInsets.only(top: 10.0, bottom: 20),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Container(
-                          height: 250.0,
-                          width: 180,
-                          decoration: BoxDecoration(
-                              // color: Colors.white,
-                              borderRadius: BorderRadius.circular(15.0),
-                              border: Border.all(color: Colors.grey)),
-                          child: Column(
-                            children: <Widget>[
-                              Container(
-                                  width: 100,
-                                  child: Image.asset('assets/image/orange.png')),
-                              Row(
-                                children: <Widget>[
-                                  Padding(
-                                    padding: const EdgeInsets.only(left: 20),
-                                    child: Text(
-                                      dataFromDB[index - 1]['username'],
-                                      style: TextStyle(fontSize: 20),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  );
+                  return 
+                  Text(dataFromDB[index - 1]['username']);
+                  // Padding(
+                  //   padding: const EdgeInsets.only(top: 10.0, bottom: 20),
+                  //   child: Row(
+                  //     mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  //     children: <Widget>[
+                  //       Container(
+                  //         height: 250.0,
+                  //         width: 180,
+                  //         decoration: BoxDecoration(
+                  //             borderRadius: BorderRadius.circular(15.0),
+                  //             border: Border.all(color: Colors.grey)),
+                  //         child: Column(
+                  //           children: <Widget>[
+                  //             Container(
+                  //                 width: 100,
+                  //                 child:
+                  //                     Image.asset('assets/image/orange.png')),
+                  //             Row(
+                  //               children: <Widget>[
+                  //                 Padding(
+                  //                   padding: const EdgeInsets.only(left: 20),
+                  //                   child: Text(
+                  //                     dataFromDB[index - 1]['username'],
+                  //                     style: TextStyle(fontSize: 20),
+                  //                   ),
+                  //                 ),
+                  //               ],
+                  //             ),
+                  //             Row(
+                  //               children: <Widget>[
+                  //                 Padding(
+                  //                   padding:
+                  //                       const EdgeInsets.only(left: 20, top: 5),
+                  //                   child: Text(
+                  //                     dataFromDB[index - 1]['city'],
+                  //                     style: TextStyle(fontSize: 15),
+                  //                   ),
+                  //                 ),
+                  //               ],
+                  //             ),
+                  //             SizedBox(height: 20.0),
+                  //             Row(
+                  //               mainAxisAlignment:
+                  //                   MainAxisAlignment.spaceAround,
+                  //               children: <Widget>[
+                  //                 Text(
+                  //                   "Rs 200",
+                  //                   style: TextStyle(fontSize: 20),
+                  //                 ),
+                  //                 Text(
+                  //                   "1 kg",
+                  //                   style: TextStyle(fontSize: 20),
+                  //                 )
+                  //               ],
+                  //             ),
+                  //             SizedBox(height: 16),
+                  //             Container(
+                  //               height: 32,
+                  //               width: 80,
+                  //               decoration: BoxDecoration(
+                  //                   borderRadius: BorderRadius.circular(5.0),
+                  //                   border: Border.all(color: Colors.grey)),
+                  //               child: RaisedButton(
+                  //                 color: Colors.white,
+                  //                 onPressed: () {
+                  //                   Navigator.push(
+                  //                     context,
+                  //                     MaterialPageRoute(
+                  //                       builder: (_) => Test(),
+                  //                     ),
+                  //                   );
+                  //                 },
+                  //                 child: const Text('Edit',
+                  //                     style: TextStyle(
+                  //                         fontSize: 15, color: Colors.black)),
+                  //               ),
+                  //             )
+                  //           ],
+                  //         ),
+                  //       )
+                  //     ],
+                  //   ),
+                  // );
                 }
               })),
     );
