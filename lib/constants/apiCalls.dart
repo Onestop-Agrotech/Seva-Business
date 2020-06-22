8ec1f624887563d308af4036e325dabe705bb99e
Rahul_Vijay <vk.rahul318@gmail.com>
2020-06-22 09:34:19 +0530

    remove unnecessary lines

--- lib/constants/apiCalls.dart
+++ lib/constants/apiCalls.dart
@@ -1,10 +1,6 @@
 class APIService {
-  static final String _api = "https://storemanagerserver.herokuapp.com/store";
-  
-  static final String getStoreAPI = "$_api/getAllDetails/";
-
   static final String _sevaApi = "https://frozen-sands-29962.herokuapp.com/api";
-
+  
   // BUSINESSES
   static final String loginMobile = "$_sevaApi/businesses/loginMobile/";
   static final String verifyOTP = "$_sevaApi/businesses/loginMobile/verifyOTP/";
