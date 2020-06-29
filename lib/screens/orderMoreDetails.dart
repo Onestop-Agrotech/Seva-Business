d25fb98197ed6971a5ce708c8d41adb1fce7ffdb
Rahul_Vijay <vk.rahul318@gmail.com>
2020-06-29 14:12:42 +0530

    otp text field

--- lib/screens/orderMoreDetails.dart
+++ lib/screens/orderMoreDetails.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:otp_text_field/otp_text_field.dart';
+import 'package:otp_text_field/style.dart';
 import 'package:sevaBusiness/common/orderDetails.dart';
 import 'package:sevaBusiness/common/topText.dart';
+import 'package:sevaBusiness/constants/themeColors.dart';
 import 'package:sevaBusiness/models/orders.dart';
 
 class OrderMoreDetails extends StatefulWidget {
@@ -26,15 +29,11 @@ class _OrderMoreDetailsState extends State<OrderMoreDetails> {
       body: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          Text("Total Price: Rs ${widget.order.finalItemsPrice}",style: TextStyle(
-            fontSize: 18.0,
-            color: Colors.black
-          )),
+          Text("Total Price: Rs ${widget.order.finalItemsPrice}",
+              style: TextStyle(fontSize: 18.0, color: Colors.black)),
           SizedBox(height: 10.0),
-          Text("Token: ${widget.order.tokenNumber}",style: TextStyle(
-            fontSize: 16.0,
-            color: Colors.black
-          )),
+          Text("Token: ${widget.order.tokenNumber}",
+              style: TextStyle(fontSize: 16.0, color: Colors.black)),
           SizedBox(height: 10.0),
           Expanded(
             child: ListView.builder(
@@ -55,6 +54,40 @@ class _OrderMoreDetailsState extends State<OrderMoreDetails> {
           ),
         ],
       ),
+      floatingActionButton: Container(
+        color: Colors.white,
+        height: 100.0,
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0),
+              child: Text(
+                "Enter OTP:",
+                style: TextStyle(
+                    fontSize: 15.0,
+                    color: ThemeColoursSeva().dkGreen,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 12.0),
+              child: OTPTextField(
+                length: 6,
+                width: MediaQuery.of(context).size.width,
+                fieldWidth: 30,
+                style: TextStyle(fontSize: 20),
+                textFieldAlignment: MainAxisAlignment.spaceAround,
+                fieldStyle: FieldStyle.underline,
+                onCompleted: (pin) {},
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
